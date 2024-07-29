64254d20ad6969be848275c90943e94438437f7c
= <7rishi20ss@gmail.com>
2024-07-28 18:43:12 -0600

    feat: added photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,11 @@
 "use client"
 import React from 'react';
 import { Spotlight } from '@/components/ui/Spotlight';
-import { TextGenerateEffect } from './ui/text-generate-effect';
-import { TypewriterEffect } from './ui/typewriter-effect';
-// import Typewriter from 'typewriter-effect';
 import { MainTechnologies } from '@/data';
-import Typewriter from 'typewriter-effect';
-import {useTypewriter, Cursor} from "react-simple-typewriter"
-import { TypeWriter } from './type-writer';
+import { useTypewriter } from 'react-simple-typewriter';
 import { MagicButton } from './ui/magic-button';
+import Image from 'next/image';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const Hero = () => {
   const [typeEffect] = useTypewriter({
@@ -16,37 +13,47 @@ const Hero = () => {
     loop: false,
     typeSpeed: 100,
     deleteSpeed: 40,
-  })
+  });
+
   return (
-    <div className='pb-20 pt-36'>
+    <div className='pb-20 pt-36 relative'>
       <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
       <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='blue' />
       <Spotlight className='top-28 left-80 md:-left-32 h-[80vh] w-[50vw]' fill='purple' />
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-small-white/[0.03] bg-grid-small-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-    </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-small-white/[0.03] bg-grid-small-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      </div>
       <div className='flex justify-center relative my-20 z-10'>
-        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-          <h2 className=' uppercase tracking-widest text-xs text-center text-purple-200 max-w-80'>
+        <div className='max-w-[89vw]  flex flex-col items-center justify-center'>
+          <h2 className='uppercase tracking-widest text-xs text-center text-purple-200 max-w-80'>
             Made with 💜 by Rishikesh Solapure
           </h2>
-          <div className='flex items-center space-x-2 flex-wrap justify-center relative text-2xl lg:text-7xl text-center'>
-            <h1 className=' font-bold'>
-              Software engineer that excels in <span className='text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-violet-600'>{ typeEffect }</span>
-            </h1>
-            
+          <div className='flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-40'>
+            <div className='text-center'>
+                <h1 className='font-bold space-x-2 flex-wrap justify-center relative text-4xl lg:text-4xl w-full'>
+                  Software Engineer that excels in <span className='text-transparent bg-clip-text bg-gradient-to-r from-violet-400 to-violet-600'>{typeEffect}</span>
+                </h1>
+              <p className='text-center text-xs lg:text-lg py-5'>
+                Hi, I&apos;m Rishikesh Solapure, a Software Engineer passionate about developing scalable, maintainable large-scale applications.
+              </p>
+              <a href='#about' className='mt-10'>
+                <MagicButton 
+                  title='Show my work'
+                  icon={<FaLocationArrow />}
+                />
+              </a>
+            </div>
+            <div className='mt-10 md:mt-0'>
+              <Image 
+                src={"/photo1.jpeg"}
+                alt='logo'
+                width={1000}
+                height={1000}
+                className='rounded-md'
+              />
+            </div>
           </div>
-          <div className='text-transparent bg-clip-text bg-gradient-to-r from-[#FF4500] to-[#6666FF]'>
-          </div>
-          <p className='text-center text-xs lg:text-lg pt-5'>
-            Hi, I&apos;m Rishikesh Solapure a Software Engineer with passionate about developing building scalable, maintainable large scale applications
-          </p>
-          <a href='#about' className='pt-5'>
-            <MagicButton 
-              title='Show my work'
-            />
-          </a>
         </div>
       </div>
     </div>
--- components/ui/magic-button.tsx
+++ components/ui/magic-button.tsx
@@ -1,22 +1,27 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 type MagicButtonProps = {
     title: string,
     icon?: React.ReactNode,
+    position?: string,
     onClick?: () => void,
     className?: string
 }
 export const MagicButton = ({
     title,
     icon,
+    position="right",
     onClick,
     className
 }: MagicButtonProps) => {
   return (
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <span className={cn("inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-x-2", className)}>
+                { position === 'left' && icon}
                 {title}
+                { position === 'right' && icon}
             </span>
         </button>
   )
